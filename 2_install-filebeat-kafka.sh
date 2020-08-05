775477c2c7f00829786840df3f0f131680bd7ce8
imendoza <imendozak24@gmail.com>
2020-08-05 00:46:53 -0500

    Update 2_install-filebeat-kafka.sh

--- 2_install-filebeat-kafka.sh
+++ 2_install-filebeat-kafka.sh
@@ -1,4 +1,7 @@
-#!/bin/bash 
-HOSTNAME_ELASTICSEARCH="10.36.9.89:9200"
-HOSTNAME_KIBANA="10.36.9.104:30001"
-ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnameelasticsearch=$HOSTNAME_ELASTICSEARCH hostnamekibana=$HOSTNAME_KIBANA"
+#!/bin/bash
+HOSTNAME_KAFKA="'10.36.21.44:9092','10.36.21.190:9092', '10.36.21.242:9092'"
+TOPICO="filebeat-kafka-topic"
+ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnamekafka=$HOSTNAME_KAFKA topico=$TOPICO"
+#HOSTNAME_ELASTICSEARCH="10.36.9.89:9200"
+#HOSTNAME_KIBANA="10.36.9.104:30001"
+#ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnameelasticsearch=$HOSTNAME_ELASTICSEARCH hostnamekibana=$HOSTNAME_KIBANA"
