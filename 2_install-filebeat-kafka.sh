44b25ffcda1a10956980e2b40b800b7e29770b26
imendoza <imendozak24@gmail.com>
2020-08-05 00:26:59 -0500

    Update 2_install-filebeat-kafka.sh
    Programa para ejecutar Ansible Playbook

--- 2_install-filebeat-kafka.sh
+++ 2_install-filebeat-kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 HOSTNAME_ELASTICSEARCH="10.36.9.89:9200"
-HOSTNAME_KIBANA="54.89.146.182:30001"
+HOSTNAME_KIBANA="10.36.9.104:30001"
 ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnameelasticsearch=$HOSTNAME_ELASTICSEARCH hostnamekibana=$HOSTNAME_KIBANA"
