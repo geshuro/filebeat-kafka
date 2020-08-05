625b3fbc10dcf55814ef50d9c2cae329324bd100
imendoza <imendozak24@gmail.com>
2020-08-05 00:04:33 -0500

    Create 2_install-filebeat-kafka.sh
    Ejecutar Ansible Playbook para habilitar FileBeat

---
+++ 2_install-filebeat-kafka.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+HOSTNAME_ELASTICSEARCH="10.36.9.89:9200"
+HOSTNAME_KIBANA="54.89.146.182:30001"
+ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnameelasticsearch=$HOSTNAME_ELASTICSEARCH hostnamekibana=$HOSTNAME_KIBANA"
