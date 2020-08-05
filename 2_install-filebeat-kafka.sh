362f6cb724cabd772b30f3399f59ca83c6444ba1
imendoza <imendozak24@gmail.com>
2020-08-05 01:26:01 -0500

    Update 2_install-filebeat-kafka.sh

--- 2_install-filebeat-kafka.sh
+++ 2_install-filebeat-kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HOSTNAME_KAFKA="'10.36.21.44:9092','10.36.21.190:9092', '10.36.21.242:9092'"
+HOSTNAME_KAFKA="\"10.36.21.44:9092\",\"10.36.21.190:9092\", \"10.36.21.242:9092\""
 TOPICO="filebeat-kafka-topic"
 ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 playbook-filebeat-kafka.yml -i ansible_hosts -e "hostnamekafka=$HOSTNAME_KAFKA topico=$TOPICO"
 #HOSTNAME_ELASTICSEARCH="10.36.9.89:9200"
